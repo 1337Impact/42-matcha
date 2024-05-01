468835d47b9146abac77acba6878d18ab337f596
Mohammed Benkhattab <77858811+1337Impact@users.noreply.github.com>
2024-05-01 23:31:09 +0100

    Delete backend/dist directory

--- backend/dist/src/auth/authControllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = void 0;
-const login = (req, res) => {
-    console.log("Login request received");
-    res.send("Login request received");
-};
-exports.login = login;
--- backend/dist/src/auth/authRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authControllers_1 = require("./authControllers");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/login", authControllers_1.login);
-exports.default = router;
