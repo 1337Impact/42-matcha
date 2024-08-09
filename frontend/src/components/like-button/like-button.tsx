286b080f7e5e732c42b22fa42317cf55464da6c1
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-08-09 21:56:35 +0100

    feat: :sparkles: update like button

--- frontend/src/components/like-button/like-button.tsx
+++ frontend/src/components/like-button/like-button.tsx
@@ -1,25 +1,32 @@
 import axios from "axios";
-import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import { IoHeartDislikeOutline } from "react-icons/io5";
+import { toast } from "react-toastify";
 
 export default function LikeButton({ profileId }: { profileId: string }) {
   const token = window.localStorage.getItem("token");
   const handleLike = async () => {
-    await axios.post(
-      `${import.meta.env.VITE_APP_API_URL}/profile/like`,
-      {
-        profileId: profileId,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
+    try {
+      await axios.post(
+        `${import.meta.env.VITE_APP_API_URL}/profile/like`,
+        {
+          profileId: profileId,
         },
-      }
-    );
-    console.log("liked: ", profileId);
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast.success("Profile liked successfully");
+    } catch (error: any) {
+      console.error("Error liking profile:", error);
+      toast.error("Failed to like profile");
+    }
   };
   return (
     <button onClick={handleLike} className="text-red-400">
-      <FaHeart className="w-8 h-8" />
+      <FaRegHeart className="w-8 h-8" />
     </button>
   );
 }
