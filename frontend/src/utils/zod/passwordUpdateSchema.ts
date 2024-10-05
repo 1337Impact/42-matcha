aae7132255b0ca82feedec30008ffd75b228908f
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-10-05 16:58:54 +0100

    refactor: Improve password validation schema for password update

--- frontend/src/utils/zod/passwordUpdateSchema.ts
+++ frontend/src/utils/zod/passwordUpdateSchema.ts
@@ -1,15 +1,25 @@
 import { z } from "zod";
 
-export const passwordUpdateSchema = z.object({
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters long")
-    .nonempty("Password is required"),
-  confirmPassword: z
-    .string()
-    .min(8, "Password confirmation must be at least 8 characters long")
-    .nonempty("Password confirmation is required"),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"], // Error will be associated with confirmPassword
-});
+export const passwordUpdateSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(16, "Password cannot exceed 16 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[\W_]/, "Password must contain at least one special character"),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(16, "Password cannot exceed 16 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[\W_]/, "Password must contain at least one special character"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // Error will be associated with confirmPassword
+  });
